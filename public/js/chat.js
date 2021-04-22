1328c6026a4a18dd704c146aae61bd60788e079c
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2021-04-22 14:40:35 +0530

    Socket events and message rendering

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,58 @@
-io()
+const socket = io()
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        if(error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered')
+    })
+})
+
+$sendLocationButton.addEventListener('click', ()=>{
+    if(!navigator.geolocation) return alert('Geolocation not supported by your browser')
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        const { latitude , longitude } = position.coords
+        
+        const coords = {
+            latitude,
+            longitude
+        } 
+
+        socket.emit('sendLocation', coords, (error) => {
+            $sendLocationButton.removeAttribute('disabled')
+            if(error) return console.log(error)
+            console.log('Location Shared')
+        })
+    })
+
+})
